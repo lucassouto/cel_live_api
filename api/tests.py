a8adabcfe305f296c25578058a774a9bd92ae913
Lucas Souto <lucasinfologos@gmail.com>
2018-05-10 13:11:29 -0300

    Refactor tests

--- api/tests.py
+++ api/tests.py
@@ -1,23 +1,24 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import Course
 
 
 class TestViews(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.course = Course.objects.create(
-            title='Curso de Python',
-            tutor='Lucas Souto',
-            number_lessons=25,
-            workload=5.5
-        )
 
     def test_get_courses(self):
         response = self.client.get(reverse('course_list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_courses(self):
-        response = self.client.post(reverse('course_list'))
+        response = self.client.post(
+            reverse('course_list'),
+            {
+                'title': 'Curso de Python',
+                'tutor': 'Lucas Souto',
+                'number_lessons': 25,
+                'workload': 5.5
+            }
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- api/views.py
+++ api/views.py
@@ -1,14 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import status
+from .models import Course
+from .serializers import CourseSerializer
 
 
 @api_view(['GET', 'POST'])
 def course_list(request):
     if request.method == 'GET':
-        return Response(status=status.HTTP_200_OK)
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
-        return Response(status=status.HTTP_201_CREATED)
+        serializer = CourseSerializer(data=request.data)
 
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data=serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(status=status.HTTP_400_BAD_REQUEST)
