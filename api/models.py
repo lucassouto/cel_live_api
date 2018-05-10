ea7891f65b0bdc19d435d8bc44c873df8729ac4d
Lucas Souto <lucasinfologos@gmail.com>
2018-05-10 07:02:27 -0300

    Create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    tutor = models.CharField(max_length=50)
+    number_lessons = models.IntegerField()
+    workload = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=14)
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        return self.name
